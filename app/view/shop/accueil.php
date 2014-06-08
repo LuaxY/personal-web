c7ae259723b4bd7f331f35620aeb2547e257d841
LuaxY <yann.guineau@voidmx.net>
2014-06-08 16:09:22 +0200

    fix(shop): new link format
    Remove "s" of link format.

--- app/view/shop/accueil.php
+++ app/view/shop/accueil.php
@@ -7,14 +7,14 @@
         <div class="header">
             <span class="icon">&nbsp;</span>
             <h3>{{ p.name }}</h3>
-            <a href="{{ url('shop/products/' ~ p.slug) }}" class="details">voir détails</a>
+            <a href="{{ url('shop/product/' ~ p.slug) }}" class="details">voir détails</a>
         </div>
-        <a href="{{ url('shop/products/' ~ p.slug) }}" class="preview-image">
+        <a href="{{ url('shop/product/' ~ p.slug) }}" class="preview-image">
             <img src="{{ asset('images/products/' ~ p.image) }}" alt="{{ p.name }}" />
         </a>
         <div class="footer {{ shopState(p.state, p.quantity, 0) }}">
             <p class="purchasable">{{ shopState(p.state, p.quantity, 1) }}</p>
-            <a href="{{ url('shop/products/' ~ p.slug) }}" class="btn-add">Ajouter au panier</a>
+            <a href="{{ url('shop/product/' ~ p.slug) }}" class="btn-add">Ajouter au panier</a>
             <p class="price"><span>€</span>{{ p.price }}</p>
         </div>
     </div>
