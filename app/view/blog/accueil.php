4f17f22ba281b1f5d96497e63b9fbe6a507f7fcd
LuaxY <yann.guineau@voidmx.net>
2014-07-06 23:46:50 +0200

    refactor(blog): add quick link

--- app/view/blog/accueil.php
+++ app/view/blog/accueil.php
@@ -8,7 +8,7 @@
     {% for p in posts %}
     <div class="post">
         <p><img src="{{ asset('images/flags/' ~ p.lang ~ '.png') }}" class="icon" alt="{{ p.lang }}" /> <img src="{{ asset('images/flags/' ~ p.icon) }}" class="icon" alt="{{ p.name }}" /> <?php echo strtolower(date('l d F Y', strtotime("{{ p.date }}"))); ?></p>
-        <h1>{{ p.title }}</h1>
+        <h1>{{ p.title }} <a href="{{ url('blog/post/' ~ p.slug) }}"><span class="octicon octicon-link"></span></a></h1>
         {{ p.content|raw}}
         <p><a href="{{ url('blog/post/' ~ p.slug) }}#disqus_thread">commentaire(s)</a></p>
         <hr />
