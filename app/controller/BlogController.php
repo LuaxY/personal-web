bd72e6b0463b47c32407f5fbe4e83c2068c5faa1
LuaxY <yann.guineau@voidmx.net>
2014-07-29 20:46:34 +0200

    fix(blog): no redirection for not found post

--- app/controller/BlogController.php
+++ app/controller/BlogController.php
@@ -4,7 +4,6 @@ class BlogController extends Controller
 {
     function actionIndex()
     {
-        $this->request->action = "accueil";
         $this->loadModel('Post');
 
         if(!($cache = $this->Cache->get('blog/posts')))
@@ -22,31 +21,25 @@ class BlogController extends Controller
 
     function actionPost($slug = null)
     {
-        if($slug == null)
-            $this->redirect('blog');
-
-        $this->request->action = "article";
         $this->loadModel('Post');
 
         if(!($cache = $this->Cache->get('blog/'.$slug)))
         {
             $cache = current($this->Post->query("SELECT * FROM posts as p, categories AS c WHERE p.categorie = c.id AND p.slug = :slug", array(":slug" => $slug)));
-            
-            if(!$cache)
-                $this->redirect('blog/notfound');
-
             $this->Cache->write($cache, 'blog/'.$slug, 60);
         }
 
-        $d['p'] = $cache;
-        $d['extra'] = '<script type="text/javascript">$("[data-gist-id]").gist();</script>';
+        if(!$cache)
+        {
+            $this->render('blog/no-post.php');
+        }
+        else
+        {
+            $d['p'] = $cache;
+            $d['extra'] = '<script type="text/javascript">$("[data-gist-id]").gist();</script>';
 
-        $this->set($d);
-        $this->render('blog/post.php');
-    }
-
-    function actionNotfound()
-    {
-        $this->render('blog/no-post.php');
+            $this->set($d);
+            $this->render('blog/post.php');
+        }
     }
 }
